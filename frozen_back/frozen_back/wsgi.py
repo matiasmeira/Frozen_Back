d8f5107e6d54b1ce553744ef407af4731021c9bf
Giuliano1994 <gomesdasilva.giuliano@gmail.com>
2025-09-26 18:44:11 -0300

    config

--- frozen_back/frozen_back/wsgi.py
+++ frozen_back/frozen_back/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frozen_back.frozen_back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frozen_back.settings')
 
 application = get_wsgi_application()
