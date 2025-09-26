8541806c4d578f76585f57fc412ce3c58baa09c2
Giuliano1994 <gomesdasilva.giuliano@gmail.com>
2025-09-26 18:17:49 -0300

    Config APP

--- frozen_back/frozen_back/settings.py
+++ frozen_back/frozen_back/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login',
-    'empleados',
+    'frozen_back.login',
+    'frozen_back.empleados',
 ]
 
 MIDDLEWARE = [
