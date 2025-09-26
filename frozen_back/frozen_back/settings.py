1ef3d8b72c94b9322047a81658743a8de55c4850
Giuliano1994 <gomesdasilva.giuliano@gmail.com>
2025-09-26 18:13:04 -0300

    Config bbdd

--- frozen_back/frozen_back/settings.py
+++ frozen_back/frozen_back/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'frozen_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fUKjoTegeQgXKtUqgcMTbfLHDNxwxEOc',
+        'HOST': 'nozomi.proxy.rlwy.net',
+        'PORT': '57279',
     }
 }
+
 
 
 # Password validation
