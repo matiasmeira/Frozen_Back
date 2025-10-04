7f5c1e458b40ff1cefea3b50faaa1c8ad903862d
matias <matiasezequielmeira@gmail.com>
2025-10-04 00:05:31 -0300

    se modifica model Producto

--- frozen_back/productos/models.py
+++ frozen_back/productos/models.py
@@ -23,6 +23,7 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     id_tipo_producto = models.ForeignKey(TipoProducto, on_delete=models.CASCADE, db_column="id_tipo_producto")
     id_unidad = models.ForeignKey(Unidad, on_delete=models.CASCADE, db_column="id_unidad")
+    dias_duracion = models.IntegerField()
 
     class Meta:
         db_table = "producto"
