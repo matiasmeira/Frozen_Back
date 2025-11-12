7f354e3298fca97620d270aa5c78ca0ed6a63ec0
Giuliano1994 <gomesdasilva.giuliano@gmail.com>
2025-11-12 17:20:02 -0300

    Revert "calendario"
    This reverts commit 88ec1872bad8c837fa0e066368809db550bfddba.

--- frozen_back/planificacion/urls.py
+++ frozen_back/planificacion/urls.py
@@ -6,10 +6,5 @@ urlpatterns = [
     path('planificacion/', views.ejecutar_planificacion_view, name='ejecutar-planificacion'),
     path('replanificar/', views.replanificar_produccion_view, name='replanificar_produccion'),
     path('ejecutar-mrp/', views.ejecutar_planificador_view, name='ejecutar-mrp'),
-    path(
-        'calendario/', 
-        views.CalendarioPlanificacionView.as_view(), 
-        name='calendario_planificacion_feed'
-    ),
 
 ]
